7f670771674cfea8b2ca0664f5b855c6ca8b078b
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-06-02 14:36:24 -0500

    adding back due to some file issue

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT, 
+    id INT AUTO_INCREMENT NOT NULL, 
     name VARCHAR(30),
     primary key (id)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT,
@@ -19,9 +19,9 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     primary key (id)
